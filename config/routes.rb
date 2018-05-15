197660765ecb715983311340c2e1b334eeadd455
Matjaz Sawodnik <matjaz@sawodnik.com>
2018-05-15 17:10:58 +0200

    sasaas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# list
+# get '/tasks', to: 'tasks#index', as: :tasks
+# # Show
+# get '/tasks/new', to: 'tasks#new', as: :task_new
+# post '/tasks', to: 'tasks#create'
+# get '/tasks/:id', to: 'tasks#show', as: :task
+# # Update
+# get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+# patch '/tasks/:id', to: 'tasks#update'
+# # Create
+# # Edit
+# # Delete
+# delete '/tasks/:id', to: 'tasks#destroy', as: :task_destroy
+resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2018_05_15_101412) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title "
-    t.text "details "
+    t.string "title"
+    t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
